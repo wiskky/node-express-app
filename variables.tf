cfec40363b56bce4e97e46d6fff967acc3a7a699
wiskky <adeniyi.wisdom@gmail.com>
2025-09-20 16:15:53 +0100

    Update var files

--- variables.tf
+++ variables.tf
@@ -13,13 +13,13 @@ variable "docker_image_tag" {
 variable "region" {
   type        = string
   description = "AWS region to deploy resources"
-  # default     = "eu-north-1"
+  default     = "eu-north-1"
 }
 
 variable "ami_id" {
   type        = string
   description = "AMI ID for the EC2 instance"
-  # default     = "ami-0a716d3f3b16d290c"
+  default     = "ami-0a716d3f3b16d290c"
 }
 
 variable "instance_type" {
